f3ee007b6ec449ffc4deefea62af2a3508b12344
Hazel Black <hazelbdev@gmail.com>
2024-09-28 10:47:37 -0500

    finished Creating CSS cutom properties. JS file ad Co-authored-by: th3hybrid <th3hybrid@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,4 @@
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById("last-name");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
